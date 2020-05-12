4fc5c2ff98f7f14761460f1be9e78f5068be3fca
usmanjutt07 <65243441+usmanjutt07@users.noreply.github.com>
2020-05-12 13:07:23 -0700

    usmanjutt..py

--- zeroboy.py
+++ zeroboy.py
@@ -48,18 +48,18 @@ def jalan(z):
 
 ##### LOGO #####
 logo = """ -----------------------------•◈•
-(  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+(R __)\\ ____--------------_------------•◈•
+|•◈•---█----•THE - ROMEO -OFFICAL------•◈•
+|•◈•------- •||•ROMEO - KING-----------•◈•
+|__(-----\  •◈•------HEART-HACKER------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mUsman Jutt
+\033[1;96mInstagram \033[1;93m: \033[1;FlowUmsan
+\033[1;96mFacebook  \033[1;93m: \033[1;Usmanjutt07
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/usmanjutt07/usman-jutt
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mUsman Jutt
+\033[1;96mInstagram \033[1;93m: \033[1;92mFlowUsman
+\033[1;96mFacebook  \033[1;93m: \033[1;92m UsmanJutt07
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/usmanjutt07/usman-jutt
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "usman07"
+CorrectPassword = "07usman"
 
 loop = 'true'
 while (loop == 'true'):
